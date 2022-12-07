28d712b78ea556b6589f246c5eee9a5bfad653a8
sdpy695uw325fyuv <sdpy695uw325fyuv@gmail.com>
2022-12-06 23:41:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UI-Portfolio
+ Portfolio of compiled work from my undergrad UI/UX class
---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes, Navigation, Link } from 'react-router-dom';
+ 
+import Home from './Home';
+import NavBar from './components/NavBar';
+import Test from './project pages/CSI/test.js';
+import ResponsiveRedesign from './project pages/ResponsiveRedesign';
+
+
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const webpack = require('webpack'); //to access built-in plugins
+
+
+class App extends Component {
+  
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+          <NavBar />
+            <Routes>
+             <Route path="/" element={<Home/>} exact/>
+             {/* <Route exact target="_blank" path="/CSI/" element= {new HtmlWebpackPlugin({ template: {CSI} })} />new HtmlWebpackPlugin({ template: {CSI} }) */}
+             <Route path="/p" element={<Test/>} exact/>
+            <Route component={Error}/>
+           </Routes>
+           
+        </div> 
+      </BrowserRouter>
+    );
+  }
+}
+ 
+export default App;
